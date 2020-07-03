28155bc26fa9287d81785d4dedca2bc2a8e85bc1
rodrigo.amaro <rodrigo@amaro.dev>
2020-07-03 11:24:46 -0500

    Version bump in release flow

--- version.sh
+++ version.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+branch=$(git rev-parse --abbrev-ref HEAD)
+echo $branch
+
+fix=$(git rev-list --count develop..$branch)
+IFS='-' read -ra strarr <<< "$branch"
+version="${strarr[1]}".$fix
+
+changes='s/pom.version\=.*/pom.version='$version'/g'
+sed -i -e $changes navigation/gradle.properties
+
 text=$(cat navigation/gradle.properties | grep pom.version)
 IFS='=' read -ra strarr <<< "$text"
 echo "${strarr[1]}"
